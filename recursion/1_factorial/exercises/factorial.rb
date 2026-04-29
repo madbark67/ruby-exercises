9af53ceb14ef1e130c66548d8779996809544551
madbark67 <239074869+madbark67@users.noreply.github.com>
2026-04-28 23:38:19 -0400

    Complete 1_factorial

--- recursion/1_factorial/exercises/factorial.rb
+++ recursion/1_factorial/exercises/factorial.rb
@@ -1,3 +1,15 @@
+require 'pry' 
+
 def factorial(num)
+  if num < 0
+    return nil
+  end
   # write a recursive method for calculating the [factorial](https://simple.wikipedia.org/wiki/Factorial) of a number
+  if num <= 1 
+    return 1
+  else
+    return num * factorial(num-1)
+  end
 end
+
+factorial(5)
